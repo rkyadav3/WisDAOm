4f240a1ade153de9e2e7df985f7556959bec1c83
Ravi Kumar Yadav <r9414355925@gmail.com>
2025-03-30 11:00:21 +0530

    finnal commit

--- ethers.js
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-import abi from './CreditManagerABI.json'; // if you save it
-
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner();
-const contractAddress = "YOUR_DEPLOYED_CONTRACT_ADDRESS";
-
-const creditManager = new ethers.Contract(contractAddress, abi, signer);
